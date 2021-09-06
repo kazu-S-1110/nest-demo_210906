fb118d65fc1a34b0ec0e4478710c8e93f3d0ed1d
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-06 14:20:32 +0900

    serviceの機能を学習。なるほど、書きやすいし、補完も良い！

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,15 +2,16 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 //コントローラの役割はどんなURLが来たらどんな値を返すか
-@Controller()
+@Controller() //ここの括弧にURLを入れることも可能
 export class AppController {
+  // serviceで書いたものを取り込んでくれる処理
+  constructor(private readonly appService: AppService) {}
+
   //関数を定義する前にはちゃんとデコレータ（GET,PUT,POST,DELETE）をつける
   @Get('hello') //括弧内にurlを指定する
   getHell() {
-    return 'べらぼうめ、一昨日来やがれ！';
+    return this.appService.getHello();
   }
-
-  // constructor(private readonly appService: AppService) {}
 
   // @Get()
   // getHello(): string {
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
+//serviceの役割はcontrollerで長くなってしまう関数のための置き所
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'べらぼうめ、一昨日来やがれ！にゃ！';
   }
 }
