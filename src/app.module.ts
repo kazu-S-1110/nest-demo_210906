f61a8586d46b2ae37291da89df002ccab530c017
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-06 14:35:11 +0900

    nestCliで諸々生成。これもまぁ、便利だこと♪

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 // classのデコレータ
 @Module({
-  imports: [],
+  imports: [UsersModule], //cliでmoduleを追加すると自動で補完してくれる！
   controllers: [AppController],
   providers: [AppService],
 })
