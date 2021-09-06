3d334e9b8ce1cc69c0ba730172a113610c1f772a
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-07 08:18:15 +0900

    mongoDBAtlasとの接続！（mongooseを使用）

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 // classのデコレータ
 @Module({
-  imports: [UsersModule], //cliでmoduleを追加すると自動で補完してくれる！
+  // mongodbと接続するためのmongooseModuleをimportする、これが便利らしい！
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://kazuya:Rx9GnVVrRl2FKffM@cluster0.lqapm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    ),
+  ], //cliでmoduleを追加すると自動で補完してくれる！
+
   controllers: [AppController],
   providers: [AppService],
 })
