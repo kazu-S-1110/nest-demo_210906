71f4e7c06b027c29850051592c86c7d7c56f36f6
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-06 14:11:30 +0900

    コントローラで値を返す方法を学習。jsライクでめっちゃいい〜！

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+//コントローラの役割はどんなURLが来たらどんな値を返すか
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  //関数を定義する前にはちゃんとデコレータ（GET,PUT,POST,DELETE）をつける
+  @Get('hello') //括弧内にurlを指定する
+  getHell() {
+    return 'べらぼうめ、一昨日来やがれ！';
   }
+
+  // constructor(private readonly appService: AppService) {}
+
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
 }
