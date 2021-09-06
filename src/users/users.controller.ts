219ab7830b46f4cbb7359626cac9b2a2a3d4275c
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-06 18:46:12 +0900

    Postも導入してみた。めっちゃ簡単やんけ！

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Post()
+  create(@Body() createUser) {
+    //中身を分割して複数取得したい場合=> Body("username") username, Body("password")password
+    // 他の値の取得方法も色々あり @Param(id) @Query()
+    return createUser;
+  }
+}
