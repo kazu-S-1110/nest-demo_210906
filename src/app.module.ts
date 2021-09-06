504cf1118bc846541c5fc241533b9d038b0cdd41
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-07 08:23:44 +0900

    MongoDBkeyをenvに。

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,7 @@ import { UsersModule } from './users/users.module';
 // classのデコレータ
 @Module({
   // mongodbと接続するためのmongooseModuleをimportする、これが便利らしい！
-  imports: [
-    UsersModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://kazuya:Rx9GnVVrRl2FKffM@cluster0.lqapm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    ),
-  ], //cliでmoduleを追加すると自動で補完してくれる！
+  imports: [UsersModule, MongooseModule.forRoot(process.env.MONGODB_KEY)], //cliでmoduleを追加すると自動で補完してくれる！
 
   controllers: [AppController],
   providers: [AppService],
