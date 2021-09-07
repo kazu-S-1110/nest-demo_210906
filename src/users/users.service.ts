40f1bd9faba03649cc3e34abc91d5eb558c92235
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-08 08:24:59 +0900

    bcryptを使用してpasswordをハッシュ化して保存。

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/createUser.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
@@ -13,13 +14,11 @@ export class UsersService {
   async create(user: CreateUserDto) {
     const createUser = new this.userModel({
       username: user.username,
-      password: user.password,
+      //passwordをハッシュ化する
+      password: await bcrypt.hash(user.password, 12),
     });
-    try {
-      return await createUser.save();
-    } catch (err) {
-      return err.message;
-    }
+    createUser.save();
+
     // this.users.push(user); // usersの配列に引数のuserを追加する
   }
   async findAll() {
