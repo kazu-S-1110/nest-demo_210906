80ecf3bcc66e68162ad85029467b0ac2c69c5d31
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-07 13:00:27 +0900

    取得と作成の関数が作れた。書き方はjsっぽいかな。

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -10,10 +10,15 @@ export class UsersService {
   constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
   // userの配列を作る
   users: CreateUserDto[] = [];
-  create(user: CreateUserDto) {
-    this.users.push(user); // usersの配列に引数のuserを追加する
+  async create(user: CreateUserDto) {
+    const createUser = new this.userModel({
+      username: user.username,
+      password: user.password,
+    });
+    return await createUser.save();
+    // this.users.push(user); // usersの配列に引数のuserを追加する
   }
-  findAll() {
-    return this.users;
+  async findAll() {
+    return await this.userModel.find().exec();
   }
 }
